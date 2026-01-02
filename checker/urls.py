ed88e063d1effd7e093a224c1b99d6727e26fcf4
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2026-01-01 23:24:25 -0500

    Initial commit

--- checker/urls.py
+++ checker/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("register/", views.register, name="register"),
     path("logout/", views.logout_view, name="logout"),
+    path("create-checkout-session/", views.create_checkout_session, name="create_checkout_session"),
+
 
 ]
