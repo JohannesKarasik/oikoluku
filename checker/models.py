fe1a31bec643910f9c65f3d8c5823efe92ed6503
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2026-01-03 22:48:31 -0500

    Initial commit

--- checker/models.py
+++ checker/models.py
@@ -11,12 +11,11 @@ class Profile(models.Model):
         related_name="profile"
     )
     is_paying = models.BooleanField(default=False)
+    stripe_customer_id = models.CharField(max_length=255, blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"Profile({self.user.username})"
 
 
 @receiver(post_save, sender=User)
